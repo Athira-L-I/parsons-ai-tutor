216a312763287d48557e2c85f812b9aeebdc0ab8
Athira L I <Athira-L-I@users.noreply.github.com>
2025-10-29 23:25:24 +0530

    Clean up next.config.js for direct backend calls

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  async rewrites() {
-    const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL ||   process.env.NODE_ENV === 'production'    ? 'https://parsons-tutor.dedyn.io'    : 'http://localhost:8000';
-
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${API_URL}/api/:path*`,
-      },
-    ];
-  },
+  swcMinify: true,  
 }
 
 module.exports = nextConfig;
